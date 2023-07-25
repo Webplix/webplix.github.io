74f9a7fb682290e6edac3a21fadf1c40572b0ce9
Webplix <140216881+Webplix@users.noreply.github.com>
2023-07-25 21:15:33 +0800

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,5 +1,5 @@
     function con() {
-        document.getElementById("chatbotarea").style.display = "block";
+        document.getElementById("chatbot").style.display = "block";
         document.getElementById("ChatOutput").innerHTML += "<a class='no-design-a bot'><b>Webplix</b></>: ";
         currentIndex = 0;
         timer = setInterval(generateText, speedInterval);
