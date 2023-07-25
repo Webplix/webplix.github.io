5e7fae6f1b5ceb7e1d8b9f4f1ef484fdabc3a70e
Webplix <140216881+Webplix@users.noreply.github.com>
2023-07-25 19:42:54 +0800

    Update defrag.js

--- defrag.js
+++ defrag.js
@@ -3,6 +3,7 @@ function plainTagsReplacement() {
     var chatOutputDiv = document.getElementById("ChatOutput");
     var content = chatOutputDiv.innerHTML;
     content = content.replace(/\[br\]/g, "<br>");
+    content = content.replace(/\[hr\]/g, "<hr>");
     content = content.replace(/\[b\](.*?)\[\/b\]/g, "<b>$1</b>");
     content = content.replace(/\[i\](.*?)\[\/i\]/g, "<i>$1</i>");
     content = content.replace(/\[u\](.*?)\[\/u\]/g, "<u>$1</u>");
