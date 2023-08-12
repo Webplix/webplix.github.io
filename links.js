dc4f81d1050c0eb439983aafbf813355ff4b04ae
Webplix <140216881+Webplix@users.noreply.github.com>
2023-08-12 12:22:12 +0800

    Update links.js

--- links.js
+++ links.js
@@ -5,3 +5,7 @@ var kTwitter = "https://twitter.com/KhianVictory";
 var kInstagram = "https://www.instagram.com/khiandelapena/";
 var kYouTube = "https://www.youtube.com/KhianVictoryDCalderon?sub_confirmation=1";
 var kTikTok = "https://www.tiktok.com/@khian.vc";
+
+function goto(x) {
+   window.location.href = x;
+}
