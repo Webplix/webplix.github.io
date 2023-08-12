3f508724b3422fd2541dc15bd943e91d9c0645db
Webplix <140216881+Webplix@users.noreply.github.com>
2023-08-12 12:22:29 +0800

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,6 +1,19 @@
     function con() {
-        document.getElementById("chatbot").style.display = "block";
-        document.getElementById("ChatOutput").innerHTML += "<a class='no-design-a bot'><b>Webplix</b></>: ";
+        document.getElementById("container").style.display = "flex";
+        document.getElementById("menu-buttons").style.display = "block";
+        document.getElementById("ChatOutput").innerHTML += "<a class='no-design-a bot'><b>Bot</b></>: ";
         currentIndex = 0;
         timer = setInterval(generateText, speedInterval);
     }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const elements = document.querySelectorAll(".animate-delay");
+      
+        elements.forEach(element => {
+          const delay = parseFloat(element.getAttribute("delay"));
+          setTimeout(() => {
+            element.style.animation = "webplixOfferListsTextAnimation 1s ease-out forwards";
+            element.style.opacity = "1";
+          }, delay * 1000);
+        });
+      });
